6702f7ef9da3409b7aa1cf0fa1d61069bd35c2bc
macontini <macontini@deloitte.it>
2026-03-17 12:04:39 +0100

    feat: spostamento di gran parte delle misure DAX in Python, per alleggerire il motore Power BI incapace di gestirle

--- pipeline/preprocessing_bi.py
+++ pipeline/preprocessing_bi.py
@@ -14,45 +14,72 @@ def run(input_file: Path, output_file: Path) -> None:
     logger.info(f"Lettura file {input_file}")
     df = pd.read_parquet(input_file)
 
+    # --------------------
+    # ROUNDING
+    # --------------------
+    df = df.round({
+        'temp_celsius': 2,
+        'speed_kmh': 2,
+        'latitude': 3,
+        'longitude': 3,
+        'signal_strength_dbm': 2,
+        'power_consumption_kw': 2,
+        'speed_modifier': 2,
+        'hour_noise': 2,
+        'signal_malus': 2,
+    })
+
     # Potenzialmente ridondante, ma per sicurezza si tiene
     df['timestamp'] = pd.to_datetime(df['timestamp'])
 
+    # --------------------
+    # CHIAVI DIMENSIONALI
+    # granularità al minuti
+    # --------------------
     logger.info("Generazione chiavi dimensionali `DateKey` e `TimeKey`")
     df['DateKey'] = df['timestamp'].dt.strftime("%Y%m%d").astype('int32')
     df['TimeKey'] = (
-        df['timestamp'].dt.hour * 3600
-        + df['timestamp'].dt.minute * 60
-        + df['timestamp'].dt.second
+        df['timestamp'].dt.hour * 60
+        + df['timestamp'].dt.minute
     ).astype('int32')
 
-    logger.info("Calcolo tempo assoluto (Epoch) per logiche DAX.")
-    epoch_start = pd.Timestamp(config['epoch_start'])
-    df['EpochSeconds'] = (df['timestamp'] - epoch_start).dt.total_seconds().astype('int64')
-
+    # --------------------
+    # DEDUPLICAZIONE
+    # --------------------
     logger.info("Deduplicazione per `train_id` e `timestamp`")
+    df.sort_values(by=['train_id', 'timestamp'], ascending=[True, True], inplace=True)
     df.drop_duplicates(subset=['train_id', 'timestamp'], inplace=True)
 
-    logger.info("Costruzione indice riga `RowIndexTrain`")
-    df.sort_values(by=['train_id', 'timestamp'], ascending=[True, True], inplace=True)
-    df['RowIndexTrain'] = df.groupby('train_id').cumcount() + 1
+    # --------------------
+    # DELTA (Sostituisce misure in DAX, pesanti)
+    # --------------------
+    logger.info("Calcolo Delta Temperatura e Delta Timestamp (minuti) per ciascun treno.")
+    df['delta_temp_celsius'] = df\
+        .groupby(by='train_id')['temp_celsius']\
+        .diff()\
+        .round(2)
+    df['delta_time_min'] = (df\
+        .groupby(by='train_id')['timestamp']\
+        .diff()\
+        .dt.total_seconds() / 60.0
+    ).round(2)
 
-    logger.info("Costruzione indice riga `RowIndexTower`")
-    df.sort_values(by=['cell_tower_id', 'timestamp'], ascending=[True, True], inplace=True)
-    df['RowIndexTower'] = df.groupby('cell_tower_id').cumcount() + 1
+    logger.info("Calcolo Delta Segnale (assoluto) per ciascuna torre.")
+    df.sort_values(by=['cell_tower_id', 'timestamp'], inplace=True)
+    df['delta_signal_dbm'] = df\
+        .groupby(by='cell_tower_id')['signal_strength_dbm']\
+        .diff()\
+        .abs()\
+        .round(2)
 
+    # --------------------
+    # QUALITY
+    # --------------------
     logger.info("Costruzione colonne ausiliarie di Data Quality e suddivisione velocità in categorie.")
-    df['IsMissingTemp'] = np.where(
-        df['temp_celsius'].isna(),
-        1,
-        0
-    )
+    df['IsMissingTemp'] = np.where(df['temp_celsius'].isna(), 1, 0).astype('int8')
+
     MAX_CELSIUS = config['thresholds']['temp']['max']
-    df['IsExtremeTemp'] = np.where(
-        (df['IsMissingTemp'].eq(0)) & (df['temp_celsius'] > MAX_CELSIUS),
-        1,
-        0
-    )
-    df[['IsMissingTemp', 'IsExtremeTemp']] = df[['IsMissingTemp', 'IsExtremeTemp']].astype('int8')
+    df['IsExtremeTemp'] = np.where((df['IsMissingTemp'].eq(0)) & (df['temp_celsius'] > MAX_CELSIUS), 1, 0).astype('int8')
 
     df['SpeedRange'] = pd.cut(
         df['speed_kmh'],
@@ -61,12 +88,27 @@ def run(input_file: Path, output_file: Path) -> None:
                     '04: 150-200', '05: 200-250', '06: 250-300']
     ).astype('str')
 
-    logger.info("Salvataggio dataset ordinato per `train_id` e `timestamp` (ottimizzato per Power BI)")
+    # --------------------
+    # EPURAZIONE COLONNE PESANTI
+    # utili solo per costruzione dati random
+    # `timestamp` ha cardinalità assurda e deve scomparire
+    # --------------------
+    cols_to_drop = [
+        "timestamp", "latitude", "longitude",
+        "speed_modifier", "hour_noise", "signal_malus"
+    ]
+    logger.info(f"Rimozione colonne non necessarie: {', '.join(cols_to_drop)}")
+    df.drop(columns=cols_to_drop, errors='ignore', inplace=True)
+
+    # --------------------
+    # SAVING
+    # --------------------
+    logger.info("Salvataggio dataset ordinato per `train_id` e `timestamp` (ottimizzato per compressione Power BI)")
     df.sort_values(by=['DateKey', 'train_id', 'TimeKey'], inplace=True)
     df.reset_index(drop=True, inplace=True)
 
     df.to_parquet(output_file, index=False)
-    logger.info(f"Parquet per importazione in Power BI salvato in  {output_file}")
+    logger.info(f"Parquet per importazione in Power BI salvato in {output_file}")
 
 if __name__ == '__main__':
     start_time = datetime.now()
